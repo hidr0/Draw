f2dce02099ba13bc01d50207f7b376855a333832
gIliev-120 <g.iliev96@gmail.com>
2014-04-15 23:26:56 +0300

    Update picAchooseSound.java

--- Draw/src/picAchooseSound.java
+++ Draw/src/picAchooseSound.java
@@ -13,94 +13,56 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 public class picAchooseSound{
 	JFileChooser chooser;
-	File file,music;
+	File file;
+	//File music;
 	Clip clip;
 	AudioInputStream audioIn;
 	String fname;
 	float b ;
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JButton button = new JButton();
-	JSlider slide = new JSlider();
+
+	//JSlider slide ;
 	FloatControl gainControl;
 	
-	public static void main(String[] args) {
-		picAchooseSound picSound = new picAchooseSound();
-		picSound.chooseSound();
-	}
-	public void chooseSound() {
-
-		frame = new JFrame("YOO");
-		panel = new JPanel();
-		button = new JButton("Open");
-		slide = new JSlider();
-		slide.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				JSlider source = (JSlider)e.getSource();
-		        if (!source.getValueIsAdjusting()) {
-		        	int a = source.getValue();
-		        	 b = (float)a;
-		        	System.out.println(b);
-		        	
-		        }
-				
-			}
-		});
-		slide.setMajorTickSpacing(1);
-		slide.setMaximum(6);
-		
-        slide.setMinorTickSpacing(1);
-        slide.setPaintTicks(true);
-        slide.setPaintLabels(true);
-        
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				chooser = new JFileChooser();
-				chooser.showOpenDialog(null);
-				music = chooser.getSelectedFile();
-				try {
-					// Open an audio input stream.
-
-					 audioIn = AudioSystem.getAudioInputStream(music);
-					// Get a sound clip resource.
-					 clip = AudioSystem.getClip();
-					// Open audio clip and load samples from the audio input stream.
-					clip.open(audioIn);
-					gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-
-					gainControl.setValue(b);
-					
-					clip.start();
-					clip.loop(Clip.LOOP_CONTINUOUSLY);
-				} catch (UnsupportedAudioFileException w) {
-					w.printStackTrace();
-				} catch (IOException w) {
-					w.printStackTrace();
-				} catch (LineUnavailableException w) {
-					w.printStackTrace();
-				}
-
-
-			}
-		});
-		
-		panel.add(button);
-		panel.add(slide);
-		frame.add(panel);
-		frame.setSize(500, 500);
-		frame.setVisible(true);
-
 	
-
-
+	
+	public void chooseSound(){
+		
+		chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		file = chooser.getSelectedFile();
 
 
 	}
+	
+	public void playSound(){
+		try {
+			// Open an audio input stream.
 
+			 audioIn = AudioSystem.getAudioInputStream(file);
+			// Get a sound clip resource.
+			 clip = AudioSystem.getClip();
+			// Open audio clip and load samples from the audio input stream.
+			clip.open(audioIn);
+			gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+			gainControl.setValue(6.0f);
+			
+			clip.start();
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+		} catch (UnsupportedAudioFileException w) {
+			w.printStackTrace();
+		} catch (IOException w) {
+			w.printStackTrace();
+		} catch (LineUnavailableException w) {
+			w.printStackTrace();
+		}
+	}
+
+	public void setVolume(){
+		
+	}
 
 
 
